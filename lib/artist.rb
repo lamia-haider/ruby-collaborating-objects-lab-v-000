2ced0a4274a9e979231fcd2cc6280103b47a99d1
Lamia Haider <lamia.haider@gmail.com>
2020-05-20 00:02:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
 class Artist
+<<<<<<< HEAD
   attr_accessor :name, :songs
   @@all = []
 
@@ -31,4 +32,31 @@ class Artist
   def print_songs
     @songs.each{|song| puts song.name}
   end
+=======
+
+  @@all = []
+
+    attr_accessor :name, :songs
+
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
+
+    def add_song(song)
+      @songs << song
+    end
+
+    def save
+      @@all << self
+    end
+
+    def self.find_or_create_by_name(name)
+      self.all.detect {|artname| artname == name } || Artist.new(name)
+    end
+
+    def print_songs
+      self.songs.each {|artsong| puts song.name}
+    end
+>>>>>>> 4dc51329dba4b72858febf0a4925db197301dfed
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class MP3Importer
   attr_accessor :path
 
@@ -14,3 +15,27 @@ class MP3Importer
   end
 
 end
+=======
+class MP3Importer 
+  
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path 
+  end   
+  
+  def files
+    files = [] 
+    Dir.new(self.path).each do |file|
+     files << file if file.length > 4 
+     end  
+     files  
+   end     
+
+  
+  def import
+    self.files.each {|file_name| Song.new_by_filename(file_name)}
+  end   
+  
+end   
+>>>>>>> 4dc51329dba4b72858febf0a4925db197301dfed
