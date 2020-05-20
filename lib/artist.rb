4f0850823dbac1738ff252ce7b391fe1a49b5e35
Lamia Haider <lamia.haider@gmail.com>
2020-05-20 00:15:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
   attr_accessor :name
 
-  @@songs = []
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []  #each instance of artist has its own array of songs
   end
 
   def add_song(song)
