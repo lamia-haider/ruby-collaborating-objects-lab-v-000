d2c5a8efffc62c4ddd42d444561e67f5bb110eb4
Lamia Haider <lamia.haider@gmail.com>
2020-05-20 00:06:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
-class Song
 # Song class will be responsible for creating songs given each filename and sending the artist's name (a string) to the Artist class
-attr_accessor :name, :artist
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+  end
+
 
 end
